892ffee4ca6877169f51072de13ede7bd935f7b8
fiazmslm <fiaz.bilal@mslm.io>
2023-01-19 15:22:06 +0500

    parametrized constructor

--- bst/main.cpp
+++ bst/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+class BST{
+    int data;
+    BST *left,*right;
+public:
+    // Parameterized constructor.
+    BST(int);
+
+
+};
+
+// Parameterized Constructor definition.
+BST ::BST(int value)
+{
+    data = value;
+    left = right = NULL;
+}
+
 int main(){
     cout<<"Hello World";
 }
