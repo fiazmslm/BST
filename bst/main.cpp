4e9a93a0099fbc783012264ab53681bbdbb4507d
fiazmslm <fiaz.bilal@mslm.io>
2023-01-19 15:34:58 +0500

    intsert function of BST

--- bst/main.cpp
+++ bst/main.cpp
@@ -8,14 +8,36 @@ public:
     // Parameterized constructor.
     BST(int);
 
-
+    // Insert function.
+    BST* Insert(BST*, int);
 };
 
 // Parameterized Constructor definition.
-BST ::BST(int value)
+BST::BST(int value)
 {
     data = value;
     left = right = NULL;
+}
+
+BST* BST::Insert(BST* root,int value){
+    BST* newNode = new BST(value);
+
+    // First case for root.
+    if(!root){
+        return newNode;
+    }
+
+    if(value>root->data){
+        // Right case: when new value is greater.
+        root->right = Insert(root->right, value);
+    }
+    else if (value < root->data){
+        // Left case: when new value is smaller.
+        root->left = Insert(root->left, value);
+    }
+ 
+    // Return 'root' node, after insertion.
+    return root;
 }
 
 int main(){
